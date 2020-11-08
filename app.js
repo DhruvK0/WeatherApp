6cfad72f6a1d3ee2bcc9c490a838774b59825c47
DhruvK0 <46884021+DhruvK0@users.noreply.github.com>
2020-11-07 17:22:06 -0800

    Fixed API call for min/max temp

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function setQuery(evt) {
 function getResults (query) {
     fetch(`${api.baseurl}weather?q=${query}&units=metric&APPID=${api.key}`)
     .then(weather => {
-        return weather.json;
+        return weather.json();
     }).then(displayResults);
 }
 
@@ -24,6 +24,7 @@ function getResults (query) {
 function displayResults (weather) {
     let city = document.querySelector('.location .city');
     city.innerText = `${weather.name}, ${weather.sys.country}`;
+
 
     let now = new Date();
     let date = document.querySelector('.location .date');
@@ -34,7 +35,7 @@ function displayResults (weather) {
     let weather_el = document.querySelector('.current .weather');
     weather_el.innerText = weather.weather[0].main;
 
-    let hilow = document.querySelector('hi-low');
+    let hilow = document.querySelector('.hi-low');
     hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
 }
 
